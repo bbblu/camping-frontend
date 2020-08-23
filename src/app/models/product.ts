bdecd632687a4521a5e51630b73416fbd4c53449
Leon WU <leol13365@ymail.com>
2020-08-23 22:52:49 +0800

    feat(Product_Launch)HTML資料表單完成

---
+++ src/app/models/product.ts
@@ -0,0 +1,17 @@
+export interface Product {
+  id: number;
+  title:string;
+  start_date:Date;
+  end_date:Date;
+  city:string;
+  price:number;
+  coverImage:URL;
+  ProductName: string;
+  ProductQuantity: number;
+  ProductSize: string;
+  ProductUse: string;
+  ProductLink: URL;
+  ProductBrand: string;
+  Compensation: string;
+  Remarks: string;
+}
--- src/app/product_launch/product_launch.component.ts
+++ src/app/product_launch/product_launch.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Router, ActivatedRoute } from '@angular/router';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+
 import { ApiModel } from '../models/api-model';
 import { HttpService } from '../services/http.service';
 import { error } from 'selenium-webdriver';
 import { MatIconModule } from '@angular/material/icon';
-
+import { Product } from '../models/product';
+import { City, ProductGroupFilter, Type } from '../models/product-group-filter';
 
 
 @Component({
@@ -17,33 +20,67 @@ import { MatIconModule } from '@angular/material/icon';
 export class Product_launchComponent implements OnInit {
 
   productForm!: FormGroup;
+  goodsForm!:FormGroup;
   result!: ApiModel<object>;
-  public city: Array<any> = new Array();
+  productTypes: Type[] = [];
+  city!: City;
+
+  /* prodcuts: Product = {
+    id: 1,
+    title: '標題',
+    start_date:new Date(2020,08,15),
+    end_date: new Date(2020,08,20),
+    city: '台北市',
+    price: 2000,
+    coverImage: new URL("https://youtu.be/5qeY9f0hrxc"),
+    ProductName: '客廳帳篷',
+    ProductQuantity: 2,
+    ProductSize: '3m*3m*2m',
+    ProductUse: '自己搭 廢物',
+    ProductLink: new URL("https://youtu.be/5qeY9f0hrxc"),
+    ProductBrand: 'Nike',
+    Compensation: '全額',
+    Remarks: '小夫我要進去囉'
+  }; */
 
   constructor(
-    private formBuilder:FormBuilder,
+    private formBuilder: FormBuilder,
     private http: HttpClient,
-    private httpSerivce: HttpService,
+    private httpService: HttpService,
     private router: Router
-    
   ) { }
 
-  ngOnInit():void {
-    /* this.getCityData(); */
-    this.productForm=this.formBuilder.group({
-      title:[null, [Validators.required]],
-      start_date:[null, [Validators.required]],
-      end_date:[null, [Validators.required]],
-      /* area:[null, [Validators.required]], */
-      price:[null, [Validators.required,Validators.pattern('^[0-9-_]*')]]
+  ngOnInit(): void {
+    this.getCityData();
+
+    this.productForm = this.formBuilder.group({
+      title: [null, [Validators.required]],
+      start_date: [null, [Validators.required]],
+      end_date: [null, [Validators.required]],
+      city: [null, [Validators.required]],
+      cityAreaName: [null, [Validators.required]],
+      price: [null, [Validators.required]],
+      bank:[null, [Validators.required]],
+    });
+
+    this.goodsForm = this.formBuilder.group({
+      ProductName: [null, [Validators.required]],
+      coverImage: [null, [Validators.required]],
+      ProductQuantity: [null, [Validators.required]],
+      ProductSize: [null, [Validators.required]],
+      ProductUse: [null, []],
+      ProductLink: [null, []],
+      ProductBrand: [null, []],
+      Compensation: [null, []],
+      Remarks: [null, []]
     });
   }
 
-  getCityData(){
-    this.httpSerivce.getData<Object>('/city')
-      .subscribe(response =>{
-        this.result=response;
-        /* this.city = this.result.data; */
+  getCityData(): void {
+    this.httpService.getData<ProductGroupFilter>('/product-group/filter')
+      .subscribe((response: ApiModel<ProductGroupFilter>) => {
+        this.productTypes = response.data.type;
+        this.city = response.data.city;
       });
   }
 
