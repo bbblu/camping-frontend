49817fbfc34d2b2e7e8e9ab3b72ce9168093a23a
10646018 <10646018@ntub.edu.tw>
2020-09-02 21:55:58 +0800

    商品列表

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,30 +1,32 @@
 export interface ProductGroup {
-    name: string;
-    city: string;
-    price: string;
-    borrowDateRange: string;
-    contactInformation: string;
-    productArray: Array<Product>;
+  name: string;
+  city: string;
+  price: string;
+  borrowDateRange: string;
+  contactInformation: string;
+  productArray: Array<Product>;
 }
 
 export interface Product {
-    id: number;
-    type: string;
-    count: number;
-    brand: string;
-    useInformation: string;
-    brokenCompensation: string;
-    memo: string;
-    imageArray: Array<Image>;
-    relatedLinkArray:Array<RelatedLink>;
+  id: number;
+  type: string;
+  name: string;
+  count: number;
+  brand: string;
+  appearance: string;
+  useInformation: string;
+  brokenCompensation: string;
+  memo: string;
+  imageArray: Array<Image>;
+  relatedLinkArray: Array<RelatedLink>;
 }
 
 export interface Image {
-    id: number;
-    url: string;
+  id: number;
+  url: string;
 }
 
 export interface RelatedLink {
-    id: number;
-    url: string;
+  id: number;
+  url: string;
 }
