e89f7f455417fdfd651884a195b8d1c0de184f06
Billy Huang <10646003@ntub.edu.tw>
2020-09-03 00:47:00 +0800

    新增 - auth guard 完成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './guards/auth.guard';
+
 import { UserInfoComponent } from './user-info/user-info.component';
 import { FAQComponent } from './FAQ/FAQ.component';
 import { LoginComponent } from './login/login.component';
@@ -8,9 +10,9 @@ import { RegisterComponent } from './register/register.component';
 
 
 const routes: Routes = [
-  { path: 'login' , component: LoginComponent },
-  { path: 'register' , component: RegisterComponent },
-  { path: 'user-info', component: UserInfoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'user-info', component: UserInfoComponent, canActivate: [AuthGuard] },
   { path: 'FAQ', component: FAQComponent },
 ];
 
