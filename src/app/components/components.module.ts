1f82b67d913a9b71d1f6047a35f575a8b046877b
Billy Huang <10646003@ntub.edu.tw>
2020-09-28 01:48:23 +0800

    refactor(image-cropper-dialog): move into components folder

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,13 +4,20 @@ import { RouterModule } from '@angular/router';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@modules/material.module';
+import { ImageCropperModule } from 'ngx-image-cropper';
 
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ImageCropperDialogComponent } from './image-cropper-dialog/image-cropper-dialog.component';
 
 @NgModule({
   declarations: [TopBarComponent, ImageCropperDialogComponent],
-  imports: [CommonModule, RouterModule, FlexLayoutModule, MaterialModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+    MaterialModule,
+    ImageCropperModule,
+  ],
   exports: [TopBarComponent, ImageCropperDialogComponent],
 })
 export class ComponentsModule {}
