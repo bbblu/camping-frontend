6e5be9090b8d70a0e239a004377038723fa23bc3
Billy Huang <10646003@ntub.edu.tw>
2020-09-28 02:32:36 +0800

    refactor(faq-dialog): move into pages folder

--- src/app/pages/faq/faq-dialog/faq-dialog.component.ts
+++ src/app/pages/faq/faq-dialog/faq-dialog.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface DialogData {
+  name: string;
+}
 
 @Component({
   selector: 'app-faq-dialog',
   templateUrl: './faq-dialog.component.html',
-  styleUrls: ['./faq-dialog.component.scss']
+  styleUrls: ['./faq-dialog.component.scss'],
 })
 export class FaqDialogComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<FaqDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
-
 }
