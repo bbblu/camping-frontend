60f228411ece7f91dcd6554c9f3d9d84772625bf
Billy Huang <10646003@ntub.edu.tw>
2020-09-28 02:32:47 +0800

    refactor(faq-list): move into pages folder

--- src/app/pages/faq/faq-list/faq-list.component.ts
+++ src/app/pages/faq/faq-list/faq-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MatDialog } from '@angular/material/dialog';
+
+import { FaqDialogComponent } from '../faq-dialog/faq-dialog.component';
+
 @Component({
   selector: 'app-faq-list',
   templateUrl: './faq-list.component.html',
-  styleUrls: ['./faq-list.component.scss']
+  styleUrls: ['./faq-list.component.scss'],
 })
 export class FaqListComponent implements OnInit {
+  panelOpenState = false;
+  name = 'name';
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(FaqDialogComponent, {
+      width: '70%',
+      height: '50%',
+      data: { name: this.name },
+    });
   }
-
 }
