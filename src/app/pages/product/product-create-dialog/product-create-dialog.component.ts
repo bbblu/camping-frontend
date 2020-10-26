0773280fe5708453db7e634e2320359956676f7d
knightizumi <10646006@ntub.edu.tw>
2020-10-26 20:24:30 +0800

    新增 - product-create-dialog

---
+++ src/app/pages/product/product-create-dialog/product-create-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-create-dialog',
+  templateUrl: './product-create-dialog.component.html',
+  styleUrls: ['./product-create-dialog.component.scss']
+})
+export class ProductCreateDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
