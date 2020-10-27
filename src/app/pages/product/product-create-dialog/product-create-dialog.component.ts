faed5599ef171e8c16154739f39a2c99207befce
knightizumi <10646006@ntub.edu.tw>
2020-10-27 11:17:29 +0800

    修改 - product-create
    修改 - product-create & product-create-dialog

--- src/app/pages/product/product-create-dialog/product-create-dialog.component.ts
+++ src/app/pages/product/product-create-dialog/product-create-dialog.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { MatDialog } from '@angular/material/dialog';
+import * as moment from 'moment';
+
+import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
+import { filter, map, share, takeUntil } from 'rxjs/operators';
+
+import { ProductType } from '@models/product/product-group-filter.model';
+import { City } from '@models/city/city.model';
+import { Image } from '@models/product/image.model';
+
+import { ProductService } from '@services/api/product.service';
+import { CityService } from '@services/api/city.service';
+import { SnakeBarService } from '@services/ui/snake-bar.service';
+
+import { ImageCropperDialogComponent } from '@components/image-cropper-dialog/image-cropper-dialog.component';
 @Component({
   selector: 'app-product-create-dialog',
   templateUrl: './product-create-dialog.component.html',
   styleUrls: ['./product-create-dialog.component.scss']
 })
 export class ProductCreateDialogComponent implements OnInit {
+  productForm!: FormGroup;
+  productTypes$!: Observable<ProductType[]>;
+  productImages: Image[] = [];
+  imageIndex = 0;
 
-  constructor() { }
 
+  constructor(private formBuilder: FormBuilder) {}
+  
   ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      type: [null, [Validators.required]],
+      count: [null, [Validators.required]],
+      brand: [null, [Validators.required]],
+      appearance: [null, [Validators.required]],
+      useInformation: [null, [Validators.required]],
+      brokenCompensation: [null, [Validators.required]],
+      relatedLink: [null],
+      memo: [null],
+      imageArray: [null],
+    });
   }
 
 }
