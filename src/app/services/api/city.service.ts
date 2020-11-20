7a96457e953b8bfd826d99972b961a4bf19c3aac
Billy Huang <10646003@ntub.edu.tw>
2020-11-21 05:04:14 +0800

    fix(product-filter): with new city api

--- src/app/services/api/city.service.ts
+++ src/app/services/api/city.service.ts
@@ -13,7 +13,46 @@ import { HttpService } from '@services/http.service';
 export class CityService {
   baseUrl = '/city';
 
+  private _selectCity: string = '';
+  private _selectArea: string = '';
+  private _cities: City[] = [];
+
   constructor(private httpService: HttpService) {}
+
+  set cities(value: City[]) {
+    this._cities = value;
+  }
+
+  set selectCity(value: string) {
+    this._selectCity = value;
+  }
+
+  set selectArea(value: string) {
+    this._selectArea = value;
+  }
+
+  get cityNames(): string[] {
+    const names = this._cities.map((city) => city.name);
+    return [...new Set(names)];
+  }
+
+  get areaNames(): string[] {
+    const names = this._cities
+      .filter((city) => city.name === this._selectCity)
+      .map((city) => city.areaName);
+    return [...new Set(names)];
+  }
+
+  get areaId(): number | null {
+    const area = this._cities
+      .filter(
+        (city) =>
+          city.name === this._selectCity && city.areaName === this._selectArea
+      )
+      .shift();
+
+    return area ? area.id : null;
+  }
 
   getCity(): Observable<ApiModel<City[]>> {
     return this.httpService.get<City[]>(this.baseUrl);
