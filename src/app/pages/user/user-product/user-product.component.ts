47e265ddf23be3c11ffbcb42bdf4a4bb38d31a1a
Billy Huang <10646003@ntub.edu.tw>
2020-11-24 14:39:28 +0800

    feat(user-product): add comment and bad records

--- src/app/pages/user/user-product/user-product.component.ts
+++ src/app/pages/user/user-product/user-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { BadRecord } from '@models/user/comment-and-bad-record.model';
 import { ProductGroup } from '@models/product/product-group.model';
 
 import { UserService } from '@services/api/user.service';
@@ -15,6 +16,8 @@ import { SnakeBarService } from '@services/ui/snake-bar.service';
 export class UserProductComponent implements OnInit {
   productGroups: ProductGroup[] = [];
   account: string = '';
+  comment: number = 0;
+  badRecords: BadRecord[] = [];
   nickName: string = '';
   isEdit = true;
 
@@ -32,6 +35,7 @@ export class UserProductComponent implements OnInit {
       this.account = account;
       this.nickName = nickName;
       this.isEdit = false;
+      this.getUserCommentAndBadRecord();
       this.getUserProductGroups();
     } else {
       this.getUserInfo();
@@ -47,7 +51,24 @@ export class UserProductComponent implements OnInit {
 
         this.account = res.data.account;
         this.nickName = res.data.nickName;
+        this.getUserCommentAndBadRecord();
         this.getUserProductGroups();
+      },
+      (err) => {
+        this.snakeBarService.open(err.error.message);
+      }
+    );
+  }
+
+  getUserCommentAndBadRecord(): void {
+    this.userService.getUserCommentAndBadRecord(this.account).subscribe(
+      (res) => {
+        if (!res.result) {
+          this.snakeBarService.open(res.message);
+        }
+
+        this.comment = res.data.comment;
+        this.badRecords = res.data.badRecordArray;
       },
       (err) => {
         this.snakeBarService.open(err.error.message);
